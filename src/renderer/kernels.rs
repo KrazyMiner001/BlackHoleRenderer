1a126d4a868bf4bae5f9c26f3a6305d094241430
KrazyMiner001 <krazyminer001@outlook.com>
2026-07-20 14:09:12 +0200

    refactor: Remove old testing shaders

--- src/renderer/kernels.rs
+++ src/renderer/kernels.rs
@@ -1,16 +1,12 @@
-use crate::shader::{BasicSphere, Kerr};
+use crate::shader::Kerr;
 use egui::ColorImage;
-use std::sync::Arc;
 use glam::Vec3;
-use wgpu::util::{BufferInitDescriptor, DeviceExt};
-use wgpu::wgt::BufferDescriptor;
-use wgpu::{BindGroupDescriptor, BindGroupEntry, BufferAddress, BufferUsages, ComputePipeline, ComputePipelineDescriptor, Device, DeviceDescriptor, MapMode, Queue, RequestAdapterOptions, ShaderModule, ShaderModuleDescriptor};
+use std::sync::Arc;
+use wgpu::{Device, DeviceDescriptor, Queue, RequestAdapterOptions};
 
 pub struct Compute {
     device: Arc<Device>,
     queue: Queue,
-    test_shader: Shader,
-    sphere_shader: BasicSphere,
     kerr_shader: Kerr,
 }
 
@@ -22,119 +18,16 @@ impl Compute {
 
         let device = Arc::new(device);
 
-        let test_shader = Shader::new(wgpu::include_wgsl!("../shader/simple.wgsl"), device.clone());
-        let sphere_shader = BasicSphere::new(device.clone());
         let kerr_shader = Kerr::new(device.clone(), &queue);
 
         Self {
-            device, queue, test_shader, sphere_shader, kerr_shader,
+            device,
+            queue,
+            kerr_shader,
         }
-    }
-
-    pub async fn test_shader(&self, data: u32) -> u32 {
-        u32::from_le_bytes(
-            self.test_shader.run_simple(data.to_le_bytes(), &self.queue).await
-        )
-    }
-
-    pub async fn sphere_shader(&self, pos: Vec3, width: u32, height: u32, camera_normal: Vec3) -> ColorImage {
-        self.sphere_shader.run(&self.queue, pos, width, height, camera_normal).await
     }
 
     pub async fn kerr_shader(&self, position: Vec3, width: u32, height: u32, camera_normal: Vec3, a_value: f32) -> ColorImage {
         self.kerr_shader.run(&self.queue, position, width, height, camera_normal, a_value).await
-    }
-}
-
-struct Shader {
-    module: ShaderModule,
-    pipeline: ComputePipeline,
-    device: Arc<Device>,
-}
-
-impl Shader {
-    pub fn new(descriptor: ShaderModuleDescriptor, device: Arc<Device>) -> Self {
-        let module = device.create_shader_module(descriptor);
-        let pipeline = device.create_compute_pipeline(&ComputePipelineDescriptor {
-            label: None,
-            layout: None,
-            module: &module,
-            entry_point: None,
-            compilation_options: Default::default(),
-            cache: None,
-        });
-
-        Self {
-            module,
-            pipeline,
-            device,
-        }
-    }
-
-    pub async fn run_simple<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize>(&self, input: [u8; INPUT_SIZE], queue: &Queue) -> [u8; OUTPUT_SIZE] {
-        let input_buffer = self.device.create_buffer_init(&BufferInitDescriptor {
-            label: Some("input"),
-            contents: &input,
-            usage: BufferUsages::COPY_DST | BufferUsages::STORAGE
-        });
-        let output_buffer = self.device.create_buffer(&BufferDescriptor {
-            label: Some("output"),
-            size: OUTPUT_SIZE as BufferAddress,
-            usage: BufferUsages::COPY_SRC | BufferUsages::STORAGE,
-            mapped_at_creation: false,
-        });
-        let temp_buffer = self.device.create_buffer(&BufferDescriptor {
-            label: Some("temp"),
-            size: output_buffer.size(),
-            usage: BufferUsages::COPY_DST | BufferUsages::MAP_READ,
-            mapped_at_creation: false,
-        });
-
-        let bind_group = self.device.create_bind_group(&BindGroupDescriptor {
-            label: None,
-            layout: &self.pipeline.get_bind_group_layout(0),
-            entries: &[
-                BindGroupEntry {
-                    binding: 0,
-                    resource: input_buffer.as_entire_binding()
-                },
-                BindGroupEntry {
-                    binding: 1,
-                    resource: output_buffer.as_entire_binding()
-                }
-            ]
-        });
-
-        let mut encoder = self.device.create_command_encoder(&Default::default());
-
-        {
-            let mut pass = encoder.begin_compute_pass(&Default::default());
-            pass.set_pipeline(&self.pipeline);
-            pass.set_bind_group(0, &bind_group, &[]);
-            pass.dispatch_workgroups(1, 1, 1);
-        }
-
-        encoder.copy_buffer_to_buffer(&output_buffer, 0, &temp_buffer, 0, output_buffer.size());
-
-        queue.submit([encoder.finish()]);
-
-        let output_data = {
-            let (tx, mut rx) = tokio::sync::broadcast::channel(1);
-
-            temp_buffer.map_async(MapMode::Read, .., move |result| {
-                tx.send(result).unwrap();
-            });
-
-            self.device.poll(wgpu::PollType::wait_indefinitely()).unwrap();
-
-            rx.recv().await.unwrap().unwrap();
-
-            let data = temp_buffer.get_mapped_range(..).unwrap();
-            data.to_vec()
-        };
-
-        temp_buffer.unmap();
-
-        *output_data.as_array().unwrap()
     }
 }
--- src/shader.rs
+++ src/shader.rs
@@ -1,8 +1,6 @@
 use egui::ColorImage;
 use encase::UniformBuffer;
 use glam::{vec2, Vec3};
-use image::{GenericImageView, ImageFormat, ImageReader};
-use std::io::Cursor;
 use std::sync::Arc;
 use std::time::Instant;
 use image::imageops::FilterType;
@@ -10,144 +8,8 @@ use wgpu::util::{BufferInitDescriptor, DeviceExt};
 use wgpu::wgt::BufferDescriptor;
 use wgpu::{BufferAddress, BufferUsages, ComputePipeline, Device, Extent3d, MapMode, PollType, Queue, TexelCopyBufferInfo, TexelCopyBufferLayout, TexelCopyTextureInfo, TextureAspect, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages, TextureViewDescriptor, TextureViewDimension};
 
-pub mod basic_sphere;
 pub mod simple;
 pub mod kerr;
-
-pub struct BasicSphere {
-    device: Arc<Device>,
-    pipeline: ComputePipeline,
-}
-
-impl BasicSphere {
-    pub fn new(device: Arc<Device>) -> Self {
-        use basic_sphere::compute::*;
-        let pipeline = create_main_pipeline(&device);
-
-        Self {
-            device,
-            pipeline,
-        }
-    }
-
-    pub async fn run(&self, queue: &Queue, position: glam::Vec3, width: u32, height: u32, camera_normal: Vec3) -> ColorImage {
-        let mut uniform_buffer_bytes = UniformBuffer::new(Vec::new());
-
-        uniform_buffer_bytes
-            .write(&basic_sphere::Uniforms {
-                position,
-                radius: 1.0,
-                camera_size: vec2(3.0, 3.0),
-                camera_normal,
-            })
-            .unwrap();
-
-        let uniforms_buffer = &self.device.create_buffer_init(&BufferInitDescriptor {
-            label: Some("uniforms"),
-            contents: &uniform_buffer_bytes.into_inner(),
-            usage: BufferUsages::UNIFORM | BufferUsages::STORAGE,
-        });
-        let texture = &self.device.create_texture(&TextureDescriptor {
-            label: Some("texture"),
-            size: Extent3d {
-                width,
-                height,
-                depth_or_array_layers: 1,
-            },
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: TextureDimension::D2,
-            format: TextureFormat::Rgba8Uint,
-            usage: TextureUsages::COPY_SRC | TextureUsages::STORAGE_BINDING,
-            view_formats: &[TextureFormat::Rgba8Uint],
-        });
-        let texture_view = texture.create_view(&TextureViewDescriptor {
-            label: None,
-            format: Some(TextureFormat::Rgba8Uint),
-            dimension: Some(TextureViewDimension::D2),
-            usage: None,
-            aspect: TextureAspect::All,
-            base_mip_level: 0,
-            mip_level_count: None,
-            base_array_layer: 0,
-            array_layer_count: None,
-        });
-
-        let bind_group = basic_sphere::bind_groups::BindGroup0::from_bindings(
-            &self.device,
-            basic_sphere::bind_groups::BindGroupLayout0 {
-                uniforms: uniforms_buffer.as_entire_buffer_binding(),
-                out_data: &texture_view
-            }
-        );
-
-        let mut encoder = self.device.create_command_encoder(&Default::default());
-        let mut compute_pass = encoder.begin_compute_pass(&Default::default());
-        compute_pass.set_pipeline(&self.pipeline);
-        basic_sphere::set_bind_groups(&mut compute_pass, &bind_group);
-        compute_pass.dispatch_workgroups(
-            width.div_ceil(basic_sphere::compute::MAIN_WORKGROUP_SIZE[0]),
-            height.div_ceil(basic_sphere::compute::MAIN_WORKGROUP_SIZE[1]),
-            1
-        );
-        drop(compute_pass);
-
-        let bytes_per_row = (width * 4).div_ceil(256) * 256;
-
-        let temp_buffer = self.device.create_buffer(&BufferDescriptor {
-            label: Some("temp"),
-            size: (bytes_per_row * height) as BufferAddress,
-            usage: BufferUsages::COPY_DST | BufferUsages::MAP_READ,
-            mapped_at_creation: false,
-        });
-
-        encoder.copy_texture_to_buffer(
-            TexelCopyTextureInfo {
-                texture,
-                mip_level: 0,
-                origin: Default::default(),
-                aspect: TextureAspect::All,
-            },
-            TexelCopyBufferInfo {
-                buffer: &temp_buffer,
-                layout: TexelCopyBufferLayout {
-                    offset: 0,
-                    bytes_per_row: Some(bytes_per_row),
-                    rows_per_image: None,
-                },
-            },
-            Extent3d {
-                width,
-                height,
-                depth_or_array_layers: 1,
-            }
-        );
-
-        queue.submit([encoder.finish()]);
-
-        let output_data = {
-            let (tx, mut rx) = tokio::sync::broadcast::channel(1);
-
-            temp_buffer.map_async(MapMode::Read, .., move |result| {
-                tx.send(result).unwrap();
-            });
-
-            self.device.poll(PollType::wait_indefinitely()).unwrap();
-
-            rx.recv().await.unwrap().unwrap();
-
-            let raw_data = temp_buffer.get_mapped_range(..).unwrap();
-            raw_data
-                .to_vec()
-                .chunks(bytes_per_row as usize)
-                .flat_map(|row| { &row[0..(width * 4) as usize]})
-                .copied()
-                .collect::<Vec<u8>>()
-        };
-
-        ColorImage::from_rgba_unmultiplied([width as usize, height as usize], output_data.as_slice())
-    }
-}
 
 pub struct Kerr {
     device: Arc<Device>,
